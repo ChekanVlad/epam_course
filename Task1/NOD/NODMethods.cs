2b9ac698fafdeffa7d8240490c4b87025577ea92
Чекан Влад <vlad.gnarls@yandex.ru>
2019-11-14 22:53:16 +0300

    Euclidean override (4 and 5), gistData method

--- Task1/NOD/NODMethods.cs
+++ Task1/NOD/NODMethods.cs
@@ -28,7 +28,7 @@ namespace NOD
                     b -= a;
             }
 
-            time = (DateTime.Now - t).TotalSeconds;
+            time += (DateTime.Now - t).TotalSeconds;
             return Math.Max(a, b);
         }
 
@@ -43,11 +43,44 @@ namespace NOD
         {
             DateTime t = DateTime.Now;
             c = Math.Abs(c);
-            int nod = Euclidean(c, Euclidean(a, b, ref time), ref time);
-            time = (DateTime.Now - t).TotalSeconds;
-            return nod;
+            time += (DateTime.Now - t).TotalSeconds;
+            return Euclidean(c, Euclidean(a, b, ref time), ref time);
         }
 
+        /// <summary>
+        /// Алгоритм Евклида (для 4-х)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static int Euclidean(int a, int b, int c, int d, ref double time)
+        {
+            DateTime t = DateTime.Now;
+            d = Math.Abs(d);
+            time += (DateTime.Now - t).TotalSeconds;
+            return Euclidean(d, Euclidean(a, b, c, ref time), ref time);
+        }
+
+        /// <summary>
+        /// Алгоритм Евклида (для 5-х)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static int Euclidean(int a, int b, int c, int d, int e, ref double time)
+        {
+            DateTime t = DateTime.Now;
+            e = Math.Abs(e);
+            time += (DateTime.Now - t).TotalSeconds;
+            return Euclidean(e, Euclidean(a, b, c, d, ref time), ref time);
+        }
         /// <summary>
         /// Алгоритм Стейна
         /// </summary>
@@ -59,34 +92,61 @@ namespace NOD
             DateTime t = DateTime.Now;
             if (a == 0)
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return b;
             }
             if (b == 0)
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return a;
             }                                        
             if (a == b)
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return a;
             }            
             if (a == 1 || b == 1)
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return 1;
             }                                         
             if ((a & 1) == 0)
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return ((b & 1) == 0) ? Stein(a >> 1, b >> 1, ref time) << 1 : Stein(a >> 1, b, ref time);
             }                            
             else
             {
-                time = (DateTime.Now - t).TotalSeconds;
+                time += (DateTime.Now - t).TotalSeconds;
                 return ((b & 1) == 0) ? Stein(a, b >> 1, ref time) : Stein(b, a > b ? a - b : b - a, ref time);
             }       
+        }
+
+
+        public static double[] gistData(int a, int b, int c, int d, int e)
+        {
+            double[] timesData = new double[5];
+            //Евклид (2)
+            double time = 0;
+            int nodE2 = Euclidean(a, b, ref time);
+            timesData[0] = time;
+            //Евклид (3)
+            time = 0;
+            int nodE3 = Euclidean(a, b, c, ref time);
+            timesData[1] = time;
+            //Евклид (4)
+            time = 0;
+            int nodE4 = Euclidean(a, b, c, d, ref time);
+            timesData[2] = time;
+            //Евклид (5)
+            time = 0;
+            int nodE5 = Euclidean(a, b, c, d, e, ref time);
+            timesData[3] = time;
+            //Стейн
+            time = 0;
+            int nodS = Stein(a,b, ref time);
+            timesData[4] = time;
+            return timesData;
         }
     }
 }
--- Task1/NOD_UnitTest/UnitTest1.cs
+++ Task1/NOD_UnitTest/UnitTest1.cs
@@ -21,8 +21,29 @@ namespace NOD_UnitTest
         [TestMethod]
         public void TestMethod2()
         {
-            int a = 152, b = 57, c = 76;
-            int nodE = NODMethods.Euclidean(a, b, c);
+            int a = 81, b = 27, c = 63;
+            double time = 0;
+            int nodE = NODMethods.Euclidean(a, b, c, ref time);
+            int nod = 9;
+            Assert.AreEqual(nod, nodE);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 152, b = 57, c = 76, d = 95;
+            double time = 0;
+            int nodE = NODMethods.Euclidean(a, b, c, d, ref time);
+            int nod = 19;
+            Assert.AreEqual(nod, nodE);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = 152, b = 57, c = 76, d = 95, e = 133;
+            double time = 0;
+            int nodE = NODMethods.Euclidean(a, b, c, d, e, ref time);
             int nod = 19;
             Assert.AreEqual(nod, nodE);
         }
